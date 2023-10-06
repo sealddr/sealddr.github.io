62b09a8cc3c70fe145779167c199e76cb837755b
masakazu.kadoshita <mkadoshita@gmail.com>
2023-10-06 20:45:29 +0900

    Fix Export Internationalization (i18n)

--- next.config.js
+++ next.config.js
@@ -2,23 +2,16 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-
   output: 'export',
-   
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-   
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-   
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
-
-  }
-
-module.exports = {
-  i18n: {
-    locales: ['ja'],
-    defaultLocale: 'ja',
-  },
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 }
+ 
+module.exports = nextConfig
