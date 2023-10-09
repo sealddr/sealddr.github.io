977241527d71760363946dfc4f57b369918a03ca
masakazu.kadoshita <mkadoshita@gmail.com>
2023-10-09 22:58:58 +0900

    add header and footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import Head from 'next/head'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/globals.css';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+import { ReactNode, useEffect, useRef } from 'react';
+import { useRouter } from 'next/router';
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function RootLayout({ children }: Props) {
+  const router = useRouter();
+  const mainRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    document.title = 'sealddr\'s portfolio - ' + router.pathname;
+    if (mainRef.current) {
+      mainRef.current.style.marginTop = `${document.querySelector('header')?.clientHeight}px`;
+    }
+  }, [router.pathname]);
+
   return (
-  <>
-    <Head>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>ポートフォリオサイト準備中</title>
-    </Head>
-      {children}
-  </>
+    <div>
+      <main className='main'>{children}</main>
+      <footer className='footer'>
+        <p>&copy; 2023 sealddr</p>
+      </footer>
+    </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,25 @@
 import RootLayout from '../components/layout';
-import '../styles/globals.css';
+import '../styles/index.css'
+import { Nav, Navbar } from 'react-bootstrap';
 
 export default function Home() {
   return (
     <RootLayout>
-    <div className="container">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">sealddr's Portfolio Site</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/" className='active'>About me</Nav.Link>
+          <Nav.Link href="resume">Resume</Nav.Link>
+          <Nav.Link href="products">Products</Nav.Link>
+          <Nav.Link href="blogs">Blogs</Nav.Link>
+          <Nav.Link href="link">Link</Nav.Link>
+        </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+    <div className="container" style={{ minHeight: 'calc(100vh - 150px)' }}>
       <h1>ポートフォリオサイト準備中</h1>
       <p>現在、ポートフォリオサイトを準備中です。完成までしばらくお待ちください。</p>
     </div>
