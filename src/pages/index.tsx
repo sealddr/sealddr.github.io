ee6ed766d1a07b2f38b50aabd4a03a6fa0cfaf43
masakazu.kadoshita <mkadoshita@gmail.com>
2023-10-10 03:30:28 +0900

    crate the about page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,64 @@
 import RootLayout from '../components/layout';
 import '../styles/index.css'
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar, Container, Card } from 'react-bootstrap';
 
 export default function Home() {
   return (
     <RootLayout>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">sealddr&apos;s Portfolio Site</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="/" className='active'>About me</Nav.Link>
-          <Nav.Link href="resume">Resume</Nav.Link>
-          <Nav.Link href="products">Products</Nav.Link>
-          <Nav.Link href="blogs">Blogs</Nav.Link>
-          <Nav.Link href="link">Link</Nav.Link>
-        </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-
-    <div className="container" style={{ minHeight: 'calc(100vh - 150px)' }}>
-      <h1>ポートフォリオサイト準備中</h1>
-      <p>現在、ポートフォリオサイトを準備中です。完成までしばらくお待ちください。</p>
-    </div>
+      <header className='header'>
+        <Navbar bg="primary" data-bs-theme="dark">
+          <Container>
+          <Navbar.Brand href="#home">sealddr&apos;s Portfolio Site</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/" className='active'>About</Nav.Link>
+              <Nav.Link href="resume">Resume</Nav.Link>
+              <Nav.Link href="products">Products</Nav.Link>
+              <Nav.Link href="blogs">Blogs</Nav.Link>
+              <Nav.Link href="link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+      <Container id="under-construction" style={{minHeight: 'calc(100vh - 150px)'}}>
+        <h1>About me</h1>
+        <Card style={{ width: '24rem'}}>
+          <Card.Img variant="top" src="/images/sealddr.png" />
+          <Card.Body>
+            <Card.Title>sealddr</Card.Title>
+            <Card.Text>
+                <table>
+                  <tr>
+                    <td>address:</td>
+                    <td>Osaka city, Osaka pref.</td>
+                  </tr>
+                  <tr>
+                    <td>Job:</td>
+                    <td>ICT Architect, Researcher</td>
+                  </tr>
+                  <tr>
+                    <td>Birth:</td>
+                    <td>May, 1982</td>
+                  </tr>
+                  <tr>
+                    <td>Grade:</td>
+                    <td>M. S. in Informatics, Kyoto University</td>
+                  </tr>
+                  <tr>
+                    <td>GitHub:</td>
+                    <td>sealddr</td>
+                  </tr>
+                  <tr>
+                    <td>Twitter:</td>
+                    <td>@sealddr</td>
+                  </tr>
+                </table>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Container>
     </RootLayout>
   )
 }
