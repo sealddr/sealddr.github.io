f0d81774e824cc475dd1138dd70102057566c268
masakazu.kadoshita <mkadoshita@gmail.com>
2023-10-10 04:14:39 +0900

    move header to layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,8 +1,9 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
 
 import { ReactNode, useEffect, useRef } from 'react';
 import { useRouter } from 'next/router';
+import { Nav, Navbar, Container, Offcanvas } from 'react-bootstrap';
 
 type Props = {
   children: ReactNode;
@@ -21,6 +22,32 @@ export default function RootLayout({ children }: Props) {
 
   return (
     <div>
+      <header className='header'>
+        <Navbar bg="primary" data-bs-theme="dark" key='sm' expand='sm' className="mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#home">sealddr&apos;s Portfolio Site</Navbar.Brand>
+            <Navbar.Toggle aria-controls='offcanvasNavbar-expand-${sm}' />
+            <Navbar.Offcanvas 
+              id='offcanvasNavbar-expand-${sm}'
+              aria-labelledby='offcanvasNavbar-expand-${sm}'
+              placement='end'
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title>sealddr&apos;s Portfolio Site</Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="/" active>About</Nav.Link>
+                  <Nav.Link href="#">Resume</Nav.Link>
+                  <Nav.Link href="#">Products</Nav.Link>
+                  <Nav.Link href="#">Blogs</Nav.Link>
+                  <Nav.Link href="#">Link</Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      </header>
       <main className='main'>{children}</main>
       <footer className='footer'>
         <p>&copy; 2023 sealddr</p>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,18 @@
 import RootLayout from '../components/layout';
 import '../styles/index.css'
-import { Nav, Navbar, Container, Card, Offcanvas } from 'react-bootstrap';
+import { Container, Card, Table } from 'react-bootstrap';
 
 export default function Home() {
   return (
     <RootLayout>
-      <header className='header'>
-        <Navbar bg="primary" data-bs-theme="dark" key='sm' expand='sm' className="mb-3">
-          <Container fluid>
-            <Navbar.Brand href="#home">sealddr&apos;s Portfolio Site</Navbar.Brand>
-            <Navbar.Toggle aria-controls='offcanvasNavbar-expand-${sm}' />
-            <Navbar.Offcanvas 
-              id='offcanvasNavbar-expand-${sm}'
-              aria-labelledby='offcanvasNavbar-expand-${sm}'
-              placement='end'
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title>sealddr&apos;s Portfolio Site</Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="/" className='active'>About</Nav.Link>
-                  <Nav.Link href="resume">Resume</Nav.Link>
-                  <Nav.Link href="products">Products</Nav.Link>
-                  <Nav.Link href="blogs">Blogs</Nav.Link>
-                  <Nav.Link href="link">Link</Nav.Link>
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-      </header>
       <Container id="under-construction" style={{minHeight: 'calc(100vh - 150px)'}}>
         <h1>About me</h1>
         <Card style={{ width: '24rem'}}>
           <Card.Img variant="top" src="/images/sealddr.png" />
           <Card.Body>
             <Card.Title>sealddr</Card.Title>
-                <table>
+            <Table borderless responsive="sm">
+              <tbody>
                   <tr>
                     <td>address:</td>
                     <td>Osaka city, Osaka pref.</td>
@@ -62,7 +37,8 @@ export default function Home() {
                     <td>Twitter:</td>
                     <td>@sealddr</td>
                   </tr>
-                </table>
+                </tbody>
+            </Table>
           </Card.Body>
         </Card>
       </Container>
